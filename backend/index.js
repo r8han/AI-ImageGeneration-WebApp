cce9703443df89fe21ca1584e15e352df26bb845
Rohan Hasabe <rohanhasabe@gmail.com>
2023-01-31 21:15:23 +0530

    App is working fine now, setting up for deployment

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import express from 'express';
+
+import connectDB from './mongodb/connect.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+import postRoutes from './routes/postRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/post', postRoutes);
+app.use('/api/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
